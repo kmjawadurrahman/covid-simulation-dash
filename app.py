b44ac2f15adcecf6735691dd74b76ef2795f9934
jawadur <jawadur@gmail.com>
2020-03-20 21:56:04 +0600

    added help section

--- app.py
+++ app.py
@@ -76,10 +76,29 @@ app.layout = html.Div([
                     style={'margin-bottom': 0}),
             ], className="two columns",
         ),
-        html.Div(id='datatable-div', className="seven columns",)
+        html.Div(id='datatable-div', className="seven columns")
     ], className='row'),
     html.Div([
         html.Div(id='barcharts-div')
+    ], className='row'),
+    html.Div([
+        dcc.Markdown('''
+        This simulation is based on the analysis done in this article: [Link to article](https://medium.com/@tomaspueyo/coronavirus-act-today-or-people-will-die-f4d3d9cd99ca)
+
+        It is known that there is a lag time before an infection gets reported as a confirmed case. So, a simulation model based on total number of deaths at present, fatality rate, days from infection to death, and case doubling rate has been used to estimate the actual true cases at present day.
+
+        Number of cases that caused the deaths = Total deaths as of today / (Fatality rate (in %) / 100)
+
+        Number of times cases have doubled = Days from infection to death / Case doubling time
+
+        True cases today = Number of cases that caused the deaths \* 2^(Number of times cases have doubled)
+
+        True cases on Nth day from today = True cases today \* 2^(Nth day number / Number of times cases have doubled)
+
+        The default values of fatality rate, days from infection to death, and case doubling rate are sensible defaults determined by studies on actual data (more details in the article linked above), but feel free to tweak these values as well.
+
+        Note that this is a very simple model that makes a lot of assumptions. However, this simulation gives you an idea about how and when your hospital capacities might be pushed to their limits.
+        ''')
     ], className='row'),
 ], className='ten columns offset-by-one', style={'margin-top': 50, 'margin-bottom': 100})
 
